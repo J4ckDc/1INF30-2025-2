15cc676c463187a47617ba9d409c2519cad89172
Jack Dell <jackwilliamdelacruzcrispin@gmail.com>
2025-09-30 16:31:33 -0500

    Se han creado los proyectos MAVEN se disenhado el Model y tambien el proyecto Principal

---
+++ ProyectosJava/MAVEN/SoftProg/src/main/java/pe/edu/pucp/softprog/main/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softprog.main;
+
+import pe.edu.pucp.softprog.rrhh.model.Empleado;
+
+/**
+ *
+ * @author jack-
+ */
+public class Principal {
+    public static void main (String[] args){
+        Empleado emp = 
+                new Empleado();
+    }
+}
---
+++ ProyectosJava/MAVEN/SoftProgModel/src/main/java/pe/edu/pucp/softprog/rrhh/model/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softprog.rrhh.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jack-
+ */
+public class Persona {
+    private int idPersona;
+    private String dni;
+    private String nombre;
+    private String apellidoPaterno;
+    private char sexo;
+    private Date fechaNacimiento;
+    
+    
+}
